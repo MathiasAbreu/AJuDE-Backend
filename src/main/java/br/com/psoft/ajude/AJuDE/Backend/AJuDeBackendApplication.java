76a9aedb11375ac9c84d462985ce740947a7ef2d
Mathias Abreu <mathias@Mathias.Ubuntu>
2019-11-06 11:37:22 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/psoft/ajude/AJuDE/Backend/AJuDeBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.psoft.ajude.AJuDE.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AJuDeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AJuDeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/psoft/ajude/AJuDE/Backend/AJuDeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.psoft.ajude.AJuDE.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AJuDeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
