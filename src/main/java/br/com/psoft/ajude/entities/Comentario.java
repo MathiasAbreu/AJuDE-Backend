c18764bae59edc058ca857fb38d1856e30c89737
Mathias Abreu <mathias.trajano@ccc.ufcg.edu.br>
2019-11-18 15:49:25 -0300

    Iniciando o caso de uso 05.

--- src/main/java/br/com/psoft/ajude/entities/Comentario.java
+++ src/main/java/br/com/psoft/ajude/entities/Comentario.java
@@ -1,4 +1,31 @@
 package br.com.psoft.ajude.entities;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import javax.persistence.Entity;
+
+@Entity
 public class Comentario {
+
+    private String conteudo;
+
+    @JsonCreator
+    public Comentario(String conteudo) {
+
+        super();
+        this.conteudo = conteudo;
+    }
+
+    @JsonCreator
+    public Comentario() {
+        super();
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
 }
