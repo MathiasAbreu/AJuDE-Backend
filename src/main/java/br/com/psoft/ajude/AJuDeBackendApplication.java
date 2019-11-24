0580a80966758d97ae0e89dc634a67e4ae945345
MathiasAbreu <mathias.trajano@ccc.ufcg.edu.br>
2019-11-24 14:40:16 -0300

    Habilitação do CORS, upando o projeto sem dados no banco para poder iniciarmos os testes pelo frontend.

--- src/main/java/br/com/psoft/ajude/AJuDeBackendApplication.java
+++ src/main/java/br/com/psoft/ajude/AJuDeBackendApplication.java
@@ -1,10 +1,13 @@
 package br.com.psoft.ajude;
 
 import br.com.psoft.ajude.controllers.filters.TokenFilter;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class AJuDeBackendApplication {
@@ -17,6 +20,17 @@ public class AJuDeBackendApplication {
 		filterRB.addUrlPatterns("/ajude/usuarios","/ajude/campanhas/add");
 
 		return filterRB;
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 
 	public static void main(String[] args) {
